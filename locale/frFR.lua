d1f71ad2fef56c25e9625528f9713ba618708f98
GEOS <l-geos-x@inbox.ru>
2018-07-29 22:48:51 +0300

    add Fench locale

---
+++ locale/frFR.lua
@@ -0,0 +1,11 @@
+local L = KuiPVPConfig:Locale('enGB')
+if not L then return end
+
+L.titles = {
+    iconFaction = 'Afficher les icônes de toutes les factions',
+    iconCombat = 'Afficher l'icône de combat',
+    --iconAnchor = 'Icon Anchor',
+    iconSize = 'Taille des icônes',
+    iconOwnFaction = 'Cacher l'icône de sa propre faction'
+}
+L.tooltips = {}
